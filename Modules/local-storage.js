804da4756fd2a1300c8443335fddab4a9079f68f
Ogaga <iyaraogaga69@gmail.com>
2022-04-26 17:13:47 +0100

    fix linters error

--- Modules/local-storage.js
+++ Modules/local-storage.js
@@ -8,21 +8,21 @@ export default class Store {
       }
       return books;
     }
-  
+
     addBook = (book) => {
       const books = this.getBooks();
-  
+
       books.push(book);
       window.localStorage.setItem('booksLibrary', JSON.stringify(books));
     }
-  
+
     removeBook = (button) => {
       const books = this.getBooks();
       const parentDiv = button.parentNode;
       const myTitle = parentDiv.querySelector('.title').textContent;
       const myAuthor = parentDiv.querySelector('.author').textContent;
       const booksLeft = books.filter((book) => book.title !== myTitle && book.author !== myAuthor);
-  
+
       window.localStorage.setItem('booksLibrary', JSON.stringify(booksLeft));
     }
-  }
+}
